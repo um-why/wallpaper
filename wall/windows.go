529a72b4e40b62f03849cd0ccf1d25c3c57d1554
王海洋 <93811139@qq.com>
2020-08-15 16:56:34 +0800

    细节优化

--- wall/windows.go
+++ wall/windows.go
@@ -3,14 +3,13 @@
 package wall
 
 import (
-	"log"
 	"syscall"
 )
 
 func GetWinScreenSize(nIndex int) (i int) {
-	defer func() {
-		log.Print("无法获取屏幕分辨率")
-	}()
+	//defer func() {
+	//	log.Print("无法获取屏幕分辨率")
+	//}()
 
 	rs, _, _ := syscall.NewLazyDLL("User32.dll").NewProc("GetSystemMetrics").Call(uintptr(nIndex))
 	i = int(rs)
